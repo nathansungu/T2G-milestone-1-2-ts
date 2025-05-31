aa82b615351c0902133714897c9b6499414bfc82
nathansungu <nathanamudavi@gmail.com>
2025-05-31 18:03:52 +0300

    check negative nmber

--- milestone1.js
+++ milestone1.js
@@ -2,4 +2,8 @@
 var sum = function (a, b) { return a + b; };
 //convert minutes to seconds
 var convert = function (minutes) { return minutes * 60; };
-console.log(convert(5)); // 300
+// perimeter of a rectangle
+var perimeter = function (length, width) { return 2 * (length + width); };
+//check Negative
+var isNegative = function (num) { return num < 0 ? true : false; };
+console.log(isNegative(-5));
--- milestone1.ts
+++ milestone1.ts
@@ -5,5 +5,9 @@ const sum = (a: number, b:number):number=> a+b;
  const convert =(minutes: number):number=> minutes * 60;
  
 // perimeter of a rectangle
-
 const perimeter = (length:number, width:number):number => 2 * (length + width);
+
+//check Negative
+ const isNegative =(num: number):boolean => num < 0?true:false;
+
+ console.log(isNegative(-5));
