17f0e643738fce251ae98c7679ddabb39aa9c636
nathansungu <nathanamudavi@gmail.com>
2025-05-31 18:46:31 +0300

    greeting based on time

--- milestone1.js
+++ milestone1.js
@@ -9,7 +9,52 @@ var isNegative = function (num) { return num < 0 ? true : false; };
 //check if one can drive
 var canDrive = function (name, age) {
     var response;
-    age >= 18 ? response = "\"".concat(name, "\", is old enough to drive.") : response = "\"".concat(name, "\", is not old enough to drive yet");
+    age >= 18 ? response = "\"".concat(name, "\" is old enough to drive.") : response = "\"".concat(name, "\" is not old enough to drive yet");
     return response;
 };
-console.log(canDrive("marry", 2)); // Output: 15
+//largest number
+var findlargest = function (a, b, c) {
+    if (a > b && a > c) {
+        return a;
+    }
+    else if (b > a && b > c) {
+        return b;
+    }
+    else {
+        return c;
+    }
+};
+//BMI Calculator
+var calculateBMI = function (weight, height) {
+    var BMI = weight / (height * height);
+    var category;
+    if (BMI < 18.5) {
+        category = "Underweight";
+    }
+    else if (BMI < 24.9) {
+        category = "Normal weight";
+    }
+    else if (BMI < 29.9) {
+        category = "Overweight";
+    }
+    else {
+        category = "Obese";
+    }
+    return "Your BMI is ".concat(BMI.toFixed(2), " - ").concat(category);
+};
+//greetings based on time
+var greetUser = function (name, hour) {
+    if (hour > 5 && hour < 11) {
+        return "Good morning, ".concat(name, "!");
+    }
+    else if (hour > 11 && hour < 17) {
+        return "Good afternoon, ".concat(name, "!");
+    }
+    else if (hour > 17 && hour < 21) {
+        return "Good evening, ".concat(name, "!");
+    }
+    else {
+        return "Good night, ".concat(name, "!");
+    }
+};
+console.log(greetUser("John", 11.5));
--- milestone1.ts
+++ milestone1.ts
@@ -49,3 +49,19 @@ const calculateBMI =(weight:number, height:number):string => {
 
     return `Your BMI is ${BMI.toFixed(2)} - ${category}`;
 }
+
+//greetings based on time
+ const greetUser = (name:string, hour:number):string => {
+    if (hour>5 && hour<11) {
+        return `Good morning, ${name}!`;
+        
+    }else if(hour>11 && hour<17) {
+        return `Good afternoon, ${name}!`;
+    } else if(hour>17 && hour<21) {
+        return `Good evening, ${name}!`;
+    }else{
+        return `Good night, ${name}!`;
+    }
+ }
+
+ console.log(greetUser("John", 11.5)); 
