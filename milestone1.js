5318b1caef0631c71942b95c68e662d3eaa71250
nathansungu <nathanamudavi@gmail.com>
2025-05-31 17:31:39 +0300

    challenge 1: add two numbers

---
+++ milestone1.js
@@ -0,0 +1,3 @@
+//sum of two numbers
+var sum = function (a, b) { return a + b; };
+console.log(sum(t, 3));
---
+++ milestone1.ts
@@ -0,0 +1,4 @@
+//sum of two numbers
+const sum = (a: number, b:number)=> a+b;
+
+console.log(sum(t, 3));
