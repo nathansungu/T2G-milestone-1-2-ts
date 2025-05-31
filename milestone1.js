4046f2535dc513a195cd54788d6f9db2f5be3c5c
nathansungu <nathanamudavi@gmail.com>
2025-05-31 17:56:30 +0300

    perimeter of rectangele

--- milestone1.js
+++ milestone1.js
@@ -1,3 +1,5 @@
 //sum of two numbers
 var sum = function (a, b) { return a + b; };
-console.log(sum(t, 3));
+//convert minutes to seconds
+var convert = function (minutes) { return minutes * 60; };
+console.log(convert(5)); // 300
--- milestone1.ts
+++ milestone1.ts
@@ -1,4 +1,6 @@
 //sum of two numbers
 const sum = (a: number, b:number)=> a+b;
 
-console.log(sum(t, 3));
+//convert minutes to seconds
+ const convert =(minutes: number):number=> minutes * 60;
+ console.log(convert(5)); // 300
