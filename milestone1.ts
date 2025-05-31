0274667830afeb31f008fbc2c8226cf3e170e729
nathansungu <nathanamudavi@gmail.com>
2025-05-31 17:59:03 +0300

    perimeter of a rectangle

--- milestone1.ts
+++ milestone1.ts
@@ -1,6 +1,9 @@
 //sum of two numbers
-const sum = (a: number, b:number)=> a+b;
+const sum = (a: number, b:number):number=> a+b;
 
 //convert minutes to seconds
  const convert =(minutes: number):number=> minutes * 60;
- console.log(convert(5)); // 300
+ 
+// perimeter of a rectangle
+
+const perimeter = (length:number, width:number):number => 2 * (length + width);
