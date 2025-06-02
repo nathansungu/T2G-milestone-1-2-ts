17f901fbc2f2ea54ecab37188c6369a6f6098ef1
nathansungu <nathanamudavi@gmail.com>
2025-06-02 11:41:01 +0300

    milestone2 challenge 2: find maximum value

--- milestone_2_challenges.js
+++ milestone_2_challenges.js
@@ -3,8 +3,18 @@
 var sumOfPositives = function (n) {
     var sum = 0;
     for (var i = 0; i < n.length; i++) {
-        n[i] > 0 ? sum += n[i] : sum;
+        n[i] > 0 ? (sum += n[i]) : sum;
     }
     return sum;
 };
-console.log(sumOfPositives([2, -2, 3]));
+//find maximum value
+var findmax = function (n) {
+    var maxNumber = n[0];
+    for (var i = 1; i < n.length; i++) {
+        if (n[i] > maxNumber) {
+            maxNumber = n[i];
+        }
+    }
+    return maxNumber;
+};
+console.log(findmax([2, 5, 2, 9, 1]));
--- milestone_2_challenges.ts
+++ milestone_2_challenges.ts
@@ -1,11 +1,20 @@
 //milestone 2
 //challenge 1: sum positive
-const sumOfPositives =(n:number[]):number=>{
-    let sum:number=0;
-    for(let i:number=0; i<n.length; i++ ){
-        n[i]>0 ?sum+=n[i]:sum;
-    }
-    return sum
-}
+const sumOfPositives = (n: number[]): number => {
+  let sum: number = 0;
+  for (let i: number = 0; i < n.length; i++) {
+    n[i] > 0 ? (sum += n[i]) : sum;
+  }
+  return sum;
+};
 
-console.log(sumOfPositives([2,-2,3]))
+//find maximum value
+const findmax = (n: number[]): number => {
+  let maxNumber: number = n[0];
+  for (let i = 1; i < n.length; i++) {
+    if (n[i] > maxNumber) {
+      maxNumber = n[i];
+    }
+  }
+  return maxNumber;
+};
